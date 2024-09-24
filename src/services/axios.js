aae654d0381baf99596796b86403eff7c3d2dd3a
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2024-09-24 12:09:00 -0400

    Revert "setting enviroment"
    This reverts commit 9f1fde41c5a0cff5e4dde94a6b937323af231064.

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 import authService from "./authServices";
 
+
 const BASE_URL = import.meta.env.VITE_PROD_API_URL 
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
-    headers: {Authorization : "Bearer " + authService.getCurrentUser()}
+    // headers: {Authorization : "Bearer " + authService.getCurrentUser()}
 })
     axiosInstance.interceptors.request.use((config) => {
         return config;
